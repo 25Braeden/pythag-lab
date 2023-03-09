db9ca238163f1f795cecf0cbf984a39900aab4f2
25Braeden <125329807+25Braeden@users.noreply.github.com>
2023-03-09 19:24:15 +0000

    made valueerror

--- pythag.py
+++ pythag.py
@@ -1,9 +1,19 @@
 import math
-def pythag(a, b):
-    c = math.pow(a, 2) + math.pow(b, 2)
-    final = math.sqrt(c)
-    return final
 
-a = float(input("Enter a value for leg a: "))
-b = float(input("Enter a value for leg b: "))
-print(f"From those values, leg c is: {pythag(a, b)}")
+def pythagorean_theorem(a: float, b: float) -> float:
+    """
+    Calculate the length of the hypotenuse (c) given the lengths of the other two sides (a and b).
+    """
+    c = math.sqrt(a ** 2 + b ** 2)
+    return c
+
+while True:
+    try:
+        a = float(input("Enter a value for leg a: "))
+        b = float(input("Enter a value for leg b: "))
+        break
+    except ValueError:
+        print("Invalid input. Please enter a number for both legs.")
+
+c = pythagorean_theorem(a, b)
+print(f"From those values, leg c is: {round(c, 2)}")
